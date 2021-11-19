7bc626f8860294a9274523c3899d6603e216f9a2
SarathArrepu <a.sarath2090737@gmail.com>
2021-11-18 20:20:49 -0600

    add

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
+var Account =require('./models/account'); 
 
 
 const connectionString = process.env.MONGO_CON;
@@ -71,6 +87,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session());
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -85,6 +108,11 @@ app.use("/selector", selectorRouter);
 app.use("/addmods", addmodsRouter);
 //app.use("/costume", costumeRouter);
 app.use('/resource', resourceRouter);
+
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
